2a40d524fe8647abf2c0c9856df7ec316547e49a
Romulo <urielliberal05@gmail.com>
2026-04-14 18:44:24 -0300

    Teste 3 (falha): criando teste para verficar a transformação das letras em números

--- testa_romanos.cpp
+++ testa_romanos.cpp
@@ -5,7 +5,13 @@
 
 
 TEST_CASE( "Numeros romanos - algarismos unicos", "[romanos]" ) {
-    REQUIRE(romanos_para_decimal("I") == 0);
+    CHECK(romanos_para_decimal("I") == 1);
+    CHECK(romanos_para_decimal("V") == 5);
+    CHECK(romanos_para_decimal("X") == 10);
+    CHECK(romanos_para_decimal("L") == 50);
+    CHECK(romanos_para_decimal("C") == 100);
+    CHECK(romanos_para_decimal("D") == 500);
+    CHECK(romanos_para_decimal("M") == 1000);
 }
 
 
