80c2f6c4ce357fe28c4761882f8933cfc953cf04
Romulo <urielliberal05@gmail.com>
2026-04-14 22:21:28 -0300

    refatorando o código usando switch-case para melhorar a legibilidade do código

--- src/romanos.cpp
+++ src/romanos.cpp
@@ -3,27 +3,30 @@
 #include "romanos.hpp"
 int romanos_para_decimal(char const * num_romano)
 {
-  if(*num_romano == 'I'){
-    return 1;
+  switch(*num_romano){
+    case 'I':
+      return 1;
+      break;
+    case 'V':
+      return 5;
+      break;
+    case 'X':
+      return 10;
+      break;
+    case 'L':
+      return 50;
+      break;
+    case 'C':
+      return 100;
+      break;
+    case 'D':
+      return 500;
+      break;
+    case 'M':
+      return 1000;
+      break;
+    default:
+      return -1;
   }
-  if(*num_romano == 'V'){
-    return 5;
-  }
-  if(*num_romano == 'X'){
-    return 10;
-  }
-  if(*num_romano == 'L'){
-    return 50;
-  }
-  if(*num_romano == 'C'){
-    return 100;
-  }
-  if(*num_romano == 'D'){
-    return 500;
-  }
-  if(*num_romano == 'M'){
-    return 1000;
-  }
-  return -1;
 }
  
